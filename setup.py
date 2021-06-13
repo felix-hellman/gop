60f1be995c546eb75f5c54a90945076a22aec51d
Felix Hellman <felixhellman.ro@gmail.com>
2021-06-13 11:01:24 +0200

    Add setup, update dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='gop',
+      version='0.0.1',
+      description='Client for managing godot packages',
+      author='Felix Hellman',
+      author_email='felixhellman.ro@gmail.com',
+      packages=['gop.py']),
+      license='LICENSE.md',
+    )
