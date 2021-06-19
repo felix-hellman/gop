e57b9c6a8c910db7532e99c6f0a5171294269a86
Felix Hellman <felixhellman.ro@gmail.com>
2021-06-19 21:38:09 +0200

    Adds implmenetation of private github packages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='gop',
-    version='0.0.7',
+    version='0.0.8',
     packages=['gop'],
     url='https://gop.shrimpray.com',
     license='MIT License',
